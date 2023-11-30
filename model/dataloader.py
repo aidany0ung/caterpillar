a5280aba0184e298e46c4cb2f7cb2b43fb200f6a
Aidan Young <97195362+aidan-young@users.noreply.github.com>
2023-11-30 13:47:38 +0100

    Debug DataLoader

--- model/dataloader.py
+++ model/dataloader.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
+from rdkit import Chem
+from rdkit.Chem import rdmolops
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem.rdMolDescriptors import CalcNumRotatableBonds, CalcNumHeavyAtoms
+from scipy.sparse.csgraph import laplacian
+import scipy.linalg as la
+import pandas as pd
+from collections import defaultdict
+import numpy as np
 
 class DataLoader():
-    def __init__(self, fiftyfifty=False, modelType='gnn', addChemFeatures=False):
+    def __init__(self, filename,fiftyfifty=False, modelType='gnn', addChemFeatures=False):
+        self.filename = filename
         self.fiftyfifty = fiftyfifty
         self.modelType = modelType
         self.addChemFeatures = addChemFeatures
@@ -14,77 +25,106 @@ class DataLoader():
     def load_data(self):
         # load in data from dataset path as df
         print(os.getcwd())
-        df = pd.read_csv('../data/test.csv')
+        df = pd.read_csv(self.filename)
+        # Check thatt the columns are correct
+        if 'smile' not in df.columns and 'p_np' not in df.columns:
+            raise ValueError('Columns are not correct')
+        # Convert the SMILES strings into RDKit molecules
+        df['mol'] = df['smile'].apply(Chem.MolFromSmiles)
+        # Drop NA molecules
+        df = df.dropna(subset=['mol'])
+        df['adj'] = df['mol'].apply(Chem.rdmolops.GetAdjacencyMatrix)            
         return df
     
-    def getData(modelType):
-        if modelType == 'gnn':
-            return self.getGNNData()
-        elif modelType == 'spectral':
-            return self.getSpectralData()
-        elif modelType == 'autoencoder':
-            return self.getGNNData()
+    def getData(self):
+        if self.modelType == 'gnn':
+            self.checkfiftyfifty()
+            # Convert the adjacency matrices into a list of adjacency matrices and a list of p_np values
+            x = self.dataset['adj'].tolist()
+            y = self.dataset['p_np'].tolist()
+            # Split the data into train and test
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+            # Get the longest graph
+            longest_graph = max([len(x) for x in x_train])
+            # Reset self.dataset to original
+            self.dataset = self.load_data()
+            return x_train, y_train, x_test, y_test, longest_graph
+        if self.modelType == 'spectral':
+            default = ['mol','adj','p_np']
+            if self.addChemFeatures:
+                default.extend(['num_atoms', 'num_bonds', 'num_carbon_atoms', 'num_oxygen_atoms', 'num_nitrogen_atoms', 'num_sulfur_atoms', 'num_fluorine_atoms', 'num_chlorine_atoms', 'num_bromine_atoms', 'num_iodine_atoms', 'num_phosphorus_atoms', "ExactMass", "HBondDonorCount","HBondAcceptorCount"])
+                # Filter out all in self.dataset where ExactMass is -1
+                self.dataset = self.dataset[self.dataset['ExactMass'] != -1]
+            # get only the columns we want
+            self.dataset = self.dataset[default]
+            self.checkfiftyfifty()
+            x = []
+            y = []
+            for i in range(len(self.dataset)):
+                # Calculate the laplacian matrix
+                lap = laplacian(self.dataset['adj'].iloc[i])
+                # Calculate the eigenvectors
+                eigenvalues, eigenvectors = la.eigh(lap)
+                # Find max and min eigenvalues
+                max_eig = max(eigenvalues)
+                min_eig = min(eigenvalues)
+                #Find the indices of the max and min eigenvalues
+                max_eig_index = np.where(eigenvalues == max_eig)
+                min_eig_index = np.where(eigenvalues == min_eig)
+                # Get the eigenvectors corresponding to the max and min eigenvalues
+                max_eig_vec = eigenvectors[:,max_eig_index]
+                min_eig_vec = eigenvectors[:,min_eig_index]
+                eigs = [0] * 180
+                if len(max_eig_vec) > 90:
+                    continue
+                for i in range(len(max_eig_vec)):
+                    eigs[i] = max_eig_vec[i]
+                for i in range(len(min_eig_vec)):
+                    eigs[i+90] = min_eig_vec[i]
+                eigs.append(max_eig)
+                eigs.append(min_eig)
+                # Append the chemical features
+                if self.addChemFeatures:
+                    eigs.append(self.dataset['num_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_bonds'].iloc[i])
+                    eigs.append(self.dataset['num_carbon_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_oxygen_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_nitrogen_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_sulfur_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_fluorine_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_chlorine_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_bromine_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_iodine_atoms'].iloc[i])
+                    eigs.append(self.dataset['num_phosphorus_atoms'].iloc[i])
+                    eigs.append(self.dataset['ExactMass'].iloc[i])
+                    eigs.append(self.dataset['HBondDonorCount'].iloc[i])
+                    eigs.append(self.dataset['HBondAcceptorCount'].iloc[i])
+                # Append the list to x
+                x.append(eigs)
+                y.append(self.dataset['p_np'].iloc[i])
+            x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+            self.dataset = self.load_data()
+            # Plot a histogram of the length of eigenvectors using matplotlib
         
+            return x_train, y_train, x_test, y_test
+            
 
-    # Return x_train, y_train, x_test, y_test. the x's are a list of adjacency matrices, the y's are p_np
-    def getGNNData(self):
+    def checkfiftyfifty(self):
         if self.fiftyfifty:
             df = self.dataset
+            df = df.reset_index(drop=True)
             # Get a list of all rows with p_np == 0
             p_np_0 = df[df.p_np == 0].index.tolist()
             # Get a list of all rows with p_np == 1
             p_np_1 = df[df.p_np == 1].index.tolist()
             # Randomly sample from p_np_1 to get a list of indices
             p_np_1_sample = np.random.choice(p_np_1, size=len(p_np_0), replace=False)
-            print(max(p_np_0), max(p_np_1), max(p_np_1_sample))
+            #print(max(p_np_0), max(p_np_1), max(p_np_1_sample))
             p_np_0.extend(p_np_1_sample)
             # Filter df by the indices
             df = df.iloc[p_np_0]
             # Reset the index
             df = df.reset_index(drop=True)
             self.dataset = df
-        # Get the adjacency matrices
-        x = self.dataset['adjacency_matrix'].tolist()
-        # Get the p_np values
-        y = self.dataset['p_np'].tolist()
-        # Split the data into train and test
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-        # Get the longest graph
-        longest_graph = max([len(x) for x in x_train])
-        # Reset self.dataset to original
-        self.dataset = self.load_data()
-        return x_train, y_train, x_test, y_test, longest_graph
 
-    # Return x_train, y_train, x_test, y_test. The x's are the eig_small, eig_large, and eig_small_1, eig_small_2 columns, the y's are p_np
-    def getSpectralData(self):
-        # Get the adjacency matrices
-        default = ['eig_small', 'eig_large', 'eig_small_1', 'eig_small_2']
-        if self.addChemFeatures:
-            default.extend(['n_atoms', 'n_bonds', 'n_carbon', 'n_oxygen', 'n_nitrogen', 'n_sulfur', 'n_fluorine', 'n_chlorine', 'n_bromine', 'n_iodine', 'n_phosphorus', "ExactMass", "HBondDonorCount","HBondAcceptorCount"])
-        # Filter out all in self.dataset where ExactMass is -1
-        self.dataset = self.dataset[self.dataset['ExactMass'] != -1]
-        if self.fiftyfifty:
-            df = self.dataset
-            # Get a list of all rows with p_np == 0
-            p_np_0 = df[df.p_np == 0].index.tolist()
-            # Get a list of all rows with p_np == 1
-            p_np_1 = df[df.p_np == 1].index.tolist()
-            # Randomly sample from p_np_1 to get a list of indices
-            p_np_1_sample = np.random.choice(p_np_1, size=len(p_np_0), replace=False)
-            print(max(p_np_0), max(p_np_1), max(p_np_1_sample))
-            p_np_0.extend(p_np_1_sample)
-            # Filter df by the indices
-            df = df.iloc[p_np_0]
-            # Reset the index
-            df = df.reset_index(drop=True)
-            self.dataset = df
-        x = self.dataset[default].values.tolist()
-        # Flatten the list of lists
-        x = [item for sublist in x for item in sublist]
-        # Get the p_np values
-        y = self.dataset['p_np'].tolist()
-        # Split the data into train and test
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-        self.dataset = self.load_data()
-        return x_train, y_train, x_test, y_test
-    
+
